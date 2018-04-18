5d2c9a3b33f0bab9ed958615410c773b703d1471
Thomas <thomascaycedo19@outlook.com>
2018-04-17 22:57:29 -0500

    Se agrega espacio para la informacion

--- js/main.js
+++ js/main.js
@@ -1,52 +1,9 @@
-/*=============================================
-EL OBJETO CON LAS PROPIEDADES DEL SCROLL
-=============================================*/
-
-var ps = {
-
-    posicionScroll: 0,
-    cabezote: document.querySelector("header")
-
-}
-
-
-/*=============================================
-EL OBJETO CON LOS MÉTODOS DEL SCROLL
-=============================================*/
-
-var ms = {
-
-    inicioScroll: function() {
-
-        document.addEventListener("scroll", ms.efectoParallax)
-
-    },
-
-    efectoParallax: function() {
-
-        ps.posicionScroll = window.pageYOffset;
-
-        console.log(ps.posicionScroll);
-
-        if (ps.posicionScroll > 104) {
-
-            ps.cabezote.style.position = "fixed";
-            ps.cabezote.style.zIndex = 10;
-
-
-        } else {
-
-            ps.cabezote.style.position = "relative";
-            ps.cabezote.style.zIndex = 0;
-
-        }
-
-
-    }
-}
-
-
-ms.inicioScroll();
+/*
+MOVIMIENTO DEL SCROLL
+*/
+  $(document).ready(function(){
+    $("#cabezote").sticky({topSpacing:0});
+  });
 
 /*
 El objeto con las propiedades del formulario
@@ -186,3 +143,4 @@ var m = {
 }
 
 m.inicioFormulario();
+

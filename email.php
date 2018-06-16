e975be10d77b069ac5a3c66c48167e2c4a8673fd
Thomas <thomascaycedo19@outlook.com>
2018-06-15 22:01:31 -0500

    Se integra el calendario al php

--- email.php
+++ email.php
@@ -4,8 +4,9 @@ $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $direccion = $_POST["direccion"];
 $telefono = $_POST["telefono"];
+$calendario = $_POST["calendario"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre . "\nEmail: ". $email . "\nDireccion: ". $direccion . "\nTelefono: ". $telefono . "\nMensaje: " . $mensaje;
+$contenido = "Nombre: " . $nombre . "\nEmail: ". $email . "\nDireccion: ". $direccion . "\nTelefono: ". $telefono . "\nFecha: ". $calendario. "\nMensaje: " . $mensaje;
 mail($destino,"Contacto",$contenido);
 header("Location: http://tugruppo.com/formulario_enviado.html");
 ?> 
